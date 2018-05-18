c1bde624db0dc337350dbd26f5c95d112223cb83
mayasamoylova <mayasamoylova1@gmail.com>
2018-05-18 22:43:58 +0300

    measured done

--- src/measuredExecutionTime.js
+++ src/measuredExecutionTime.js
@@ -1,3 +1,10 @@
 global.measuredExecutionTime = function (fn) {
+  return function () {
+    console.time('execution time');
+	var result = fn.apply(this, arguments);
+	console.timeEnd('execution time');
+	return result;
+  };
 
 };
+
