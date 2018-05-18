1c16386fab9db131e4c4ed1cfe711baaa40ccdbc
mayasamoylova <mayasamoylova1@gmail.com>
2018-05-18 22:43:10 +0300

    bind done

--- src/bind.js
+++ src/bind.js
@@ -1,3 +1,7 @@
 global.bind = function (fn, context) {
-
+  var argArray = [].slice.call(arguments, 2);
+	return function () {
+	  var fnArgs = [].slice.call(arguments);
+		return fn.apply(context, argArray.concat(fnArgs));
+	}
 };
